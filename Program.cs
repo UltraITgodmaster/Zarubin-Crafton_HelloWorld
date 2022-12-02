d1ff80784bfadd8b972ed94cbb45090bfe1e3b0d
Елизавета Шолохова <elizasho1611@gmail.com>
2022-12-03 08:41:36 +1000

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zarubin_Crafton_HelloWorld
+{
+    internal class Program
+    {
+        public void Main()
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+
+        }
+    }
+}
